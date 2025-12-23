6696d121e1716081a4261e164301fcce46d8760e
Toto20087 <tomas.rodriguez.moyano@gmail.com>
2025-12-23 18:39:15 -0300

    Env variables corrected

--- operandi/next.config.ts
+++ operandi/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_SUPABASE_URL: process.env.NEXT_SUPABASE_URL,
+    NEXT_SUPABASE_ANON_KEY: process.env.NEXT_SUPABASE_ANON_KEY,
+  },
 };
 
 export default nextConfig;
